5a6f2fdd91b7db3e8782cc4d9ae00d494f37a792
prosper <prospernikuzwe23@gmail.com>
2024-12-18 13:19:51 +0200

    cs5o still ain't working

---
+++ index.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include<stdio.h>
+
+
+char c = get_char("Prompt: ");
+double d = get_double("Prompt: ");
+float f = get_float("Prompt: ");
+int i = get_int("Prompt: ");
+long l = get_long("Prompt: ");
+string s = get_string("Prompt: ");
+
+// deprecated as of fall 2017
+long long ll = get_long_long("Prompt: ");
